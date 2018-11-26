4202f2b46e051a9152e24415f70cc1f8887d43f9
Arion <arion2107@gmail.com>
2018-11-26 17:46:40 +0600

    Delete test middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,4 @@
 import {applyMiddleware, createStore} from 'redux';
 import {rootReducer} from "../reducers";
-import {ping} from './enchancers/ping';
 
 export const store = createStore(rootReducer, applyMiddleware(ping));
