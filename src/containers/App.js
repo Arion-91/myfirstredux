b9c2da8fb9b0c43917e5592a9e5ab80aea15fa63
Arion <arion2107@gmail.com>
2018-11-26 13:33:54 +0600

    Добавлены компонеты user & page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {User} from '../components/User';
+import {Page} from '../components/Page';
+
 import './App.css';
 
 class App extends Component {
     render() {
         const { user, page } = this.props;
-
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Мой топ фото</h1>
                 </header>
-                <p>Привет, {user.name}!</p>
-                <p>
-                    У тебя {page.photos.length} фото за {page.year} год
-                </p>
+                <User name={user.name} />
+                <Page photos={page.photos} year={page.year} />
             </div>
         )
     }
 }
+
 // приклеиваем данные из store
 const mapStateToProps = store => {
     console.log(store); // посмотрим, что же у нас в store?
