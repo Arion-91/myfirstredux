9cf2cd857c349297369cab70b15e8bcc1ec91149
Arion <arion2107@gmail.com>
2018-11-26 13:45:25 +0600

    Добавлены компонеты user & page

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {User} from '../components/User';
 import {Page} from '../components/Page';
+import { setYear } from '../actions/PageActions'
 
 import './App.css';
 
 class App extends Component {
     render() {
-        const { user, page } = this.props;
+        const { user, page, setYearAction } = this.props;
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Мой топ фото</h1>
                 </header>
                 <User name={user.name} />
-                <Page photos={page.photos} year={page.year} />
+                <Page photos={page.photos} year={page.year} setYear={setYearAction}/>
             </div>
         )
     }
@@ -22,12 +23,17 @@ class App extends Component {
 
 // приклеиваем данные из store
 const mapStateToProps = store => {
-    console.log(store); // посмотрим, что же у нас в store?
     return {
         user: store.user,
         page: store.page,
     }
 };
 
+const mapDispatchToProps = dispatch => {
+    return {
+        setYearAction: year => dispatch(setYear(year))
+    }
+};
+
 // в наш компонент App, с помощью connect(mapStateToProps)
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
