460b7acc0a95b01f138dfce0292f535259b8cbac
Arion <arion2107@gmail.com>
2018-11-27 13:49:07 +0600

    Добавлены константы

---
+++ src/constants/request.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const REQ_PROTOCOL = 'https://';
+export const REQ_ADRESS = 'api.vk.com/method';
+export const REQ_METHOD = 'users.get';
+export const REQ_PARAMS = 'user_id';
+export const API_VERSION = '5.52';
+
